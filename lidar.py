570e0775c0d4f99a32391a438bb585282afa078d
Russell Joyce <russell.joyce@york.ac.uk>
2015-09-26 20:06:38 +0100

    Made Lidar serial receive more robust Changes data format returned by acquire()

--- lidar.py
+++ lidar.py
@@ -22,19 +22,28 @@ class Lidar():
 
 	def laserOn(self):
 		self.ser.write("BM\n")
-		return self.ser.read(8)
+		self.ser.readline()
+		self.ser.readline()
+		self.ser.readline()
 
 	def laserOff(self):
 		self.ser.write("QT\n")
-		return self.ser.read(8)
+		self.ser.readline()
+		self.ser.readline()
 
-	def acquire(self, step):
-		self.ser.write('GD{:0>4}{:0>4}00\n'.format(step, step))
-		return self.ser.read(29)
-
-	def acquireAll(self, first, last, cluster):
-		self.ser.write('GS{:0>4}{:0>4}{:0>2}\n'.format(first, last, cluster))
-		return self.ser.read(58)
+	def acquire(self, first, last):
+		self.ser.write('GS{:0>4}{:0>4}00\n'.format(first, last))
+		self.ser.readline()
+		self.ser.readline()
+		self.ser.readline()
+		buf = ''
+		while True:
+			line = self.ser.readline()
+			if len(line) == 1:
+				break
+			else:
+				buf += line[0:-2]
+		return buf
 
 	def close(self):
 		self.ser.flushInput()
